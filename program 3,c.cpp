1e36ffdf9be1e37bbf3bacb0b07760c2b279edd6
mvijayasree <mvijayasree78@gmail.com>
2025-11-07 22:50:03 +0530

    Create program 3,c.cpp
    simple program to printf sum and diff of 2 numbers

---
+++ program 3,c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,diff;
+	printf("enter a and b values");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	diff=a-b;
+	printf("sum of digits is %d",sum);
+	printf("diff of digits is %d",diff);
+	return 0;
+}
